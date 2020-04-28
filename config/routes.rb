0a29dadd4eac289c679adaa15840858527d0edc3
koniman <user@USER-no-MacBook-Pro.local>
2020-04-28 14:32:23 +0900

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#新規投稿
-  get '/users/new', to: 'users#new'
-#新規登録
-post '/users', to: 'users#create'
-#一覧画面へ
-get '/users', to: 'users#index'
-#編集
-get '/users/:id/edit', to: 'users#edit'
-#更新
-patch '/users/:id', to: 'users#update'
 
-#削除
-delete '/users/:id', to: 'users#destroy'
+resources :users
 end
